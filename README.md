f3c687a79956763612468b3c7a9e9ff11a2c01ba
Dario Vladovic <d.vladimyr@gmail.com>
2018-12-10 14:43:21 +0100

    Implement lite tldr client
    - searching github tldr repo for pages
    - fetching page on-fly (no cache)
    - using user-defined theme from `.tldrrc`

---
+++ README.md
@@ -0,0 +1,30 @@
+# tldr-lite [![npm package version](https://badgen.net/npm/v/tldr-lite)](https://npm.im/tldr-lite) [![github license](https://badgen.net/github/license/vladimyr/tldr-lite)](https://github.com/vladimyr/tldr-lite/blob/master/LICENSE) [![js semistandard style](https://badgen.net/badge/code%20style/semistandard/pink)](https://github.com/Flet/semistandard)
+
+>lite command line `tldr` client; simplified and community-driven manpages
+
+## Run
+```
+$ npx tldr-lite [command]
+```
+
+## Usage
+```
+$ npx tldr-lite --help
+
+tldr-lite v0.0.0
+
+Usage:
+  $ tldr-lite [command]        # view tldr page for given command
+  $ tldr-lite --raw [command]  # view raw version of tldr page (markdown)
+  $ tldr-lite --web [command]  # open tldr page in browser
+  $ tldr-lite search [query]   # query tldr pages github repo
+  $ tldr-lite home             # open tldr-pages github repo
+  $ tldr-lite browse           # browse pages online
+
+Options:
+  -h, --help     Show help
+  -v, --version  Show version number
+
+Homepage:     https://github.com/vladimyr/tldr-lite
+Report issue: https://github.com/vladimyr/tldr-lite/issues
+```
---
+++ cli.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const { bold, green, red, yellow } = require('kleur');
+const { remotePaths, search } = require('./lib/search');
+const opn = require('opn');
+const Parser = require('./lib/parser');
+const pkg = require('./package.json');
+const r = require('gh-got');
+const Theme = require('./lib/theme');
+
+const platform = (name => {
+  if (name === 'darwin') return 'osx';
+  if (name === 'win32') return 'windows';
+  return name;
+})(process.platform);
+// NOTE: Copied from bili (by @egoist): https://git.io/fxupU
+const supportsEmoji = process.platform !== 'win32' ||
+                      process.env.TERM === 'xterm-256color';
+
+const emoji = char => supportsEmoji ? `${char}  ` : '';
+const formatError = msg => msg.replace(/^\w*Error:\s+/, match => red().bold(match));
+const openUrl = url => opn(url, { wait: false });
+
+const argv = require('minimist')(process.argv.slice(2), {
+  boolean: true,
+  alias: {
+    v: 'version',
+    h: 'help'
+  }
+});
+
+const help = `
+  ${bold(pkg.name)} v${pkg.version}
+
+  Usage:
+    $ ${pkg.name} [command]        # view tldr page for given command
+    $ ${pkg.name} --raw [command]  # view raw version of tldr page (markdown)
+    $ ${pkg.name} --web [command]  # open tldr page in browser
+    $ ${pkg.name} search [query]   # query tldr pages github repo
+    $ ${pkg.name} home             # open tldr-pages github repo
+    $ ${pkg.name} browse           # browse pages online
+
+  Options:
+    -h, --help     Show help
+    -v, --version  Show version number
+
+  Homepage:     ${green(pkg.homepage)}
+  Report issue: ${green(pkg.bugs.url)}
+`;
+
+program(argv._, argv).catch(err => console.error(formatError(err.stack)));
+
+async function program([command, ...tokens], flags) {
+  if (flags.version) return console.log(pkg.version);
+  if (flags.help) return console.log(help);
+  if (command === 'home') return openUrl(pkg.config.tldrRepoUrl);
+  if (command === 'browse') {
+    return openUrl(pkg.config.tldrRepoUrl + '/tree/master/pages');
+  }
+  if (command === 'search') return searchPages(normalize(tokens));
+  viewPage(command, { raw: flags.raw, useBrowser: flags.web });
+}
+
+async function viewPage(command, { raw, useBrowser } = {}) {
+  const paths = await remotePaths(command);
+  const pagePath = paths[platform] || paths[0];
+  if (!pagePath) {
+    console.error('Page not found.');
+    return console.error('\n%sFeel free to send pull request to: %s',
+      emoji('💡'), pkg.config.tldrRepoUrl);
+  }
+  if (useBrowser) {
+    return openUrl(pkg.config.tldrRepoUrl + '/blob/master/' + pagePath);
+  }
+  const pageUrl = pkg.config.tldrRepoUrl + '/raw/master/' + pagePath;
+  const resp = await fetch(pageUrl);
+  if (raw) return console.log(resp.body);
+  displayPage(resp.body);
+}
+
+async function searchPages(query) {
+  if (!query) {
+    console.error(formatError('Error: No search query provided!'));
+    return console.log(help);
+  }
+  console.log('%sSearching for: %s', emoji('🔍'), yellow(query));
+  const pages = await search(query);
+  if (!pages.length) {
+    return console.log('%sNo pages found. Try different search.', emoji('⛔️'));
+  }
+  console.log();
+  pages.map(page => console.log('  $ %s', pageInfo(page)));
+  return console.log('\n%sRun %s to see specific page.',
+    emoji('💡'), yellow(`npx ${pkg.name} <command>`));
+}
+
+async function displayPage(contents) {
+  const page = Parser.parse(contents);
+  const theme = await Theme.getSelected();
+  console.log(page.toString(theme));
+}
+
+function pageInfo(page) {
+  if (page.os[0] === 'common') return bold(page.name);
+  return `${bold(page.name)}  (Available on: ${page.os.sort().join(', ')})`;
+}
+
+function normalize(query) {
+  return query.join(' ').replace(/\s+/g, ' ')
+    .replace(/\s+or\s+/ig, op => op.toUpperCase())
+    .replace(/\s+and\s+/ig, op => op.toUpperCase());
+}
+
+function fetch(url) {
+  return r.get(url, { json: false, baseUrl: null, headers: null });
+}
